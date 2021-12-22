b68225d67c1643b9c50ec0bcd0a3a6136ed7f6be
queercat <tusek.may@gmail.com>
2021-12-22 03:47:01 -0800

    Fixed links.

--- akira-practice/src/components/akira/akira.js
+++ akira-practice/src/components/akira/akira.js
@@ -3,10 +3,10 @@ import './akira.css'
 function Akira() {
     return (
         <div className="akira">
-                <img className="akirabike akirabike-shape" src="/images/akira-bike.png"></img>
-                <img className="akirabike" src="/images/akira-bike.png"></img>            
+                <img className="akirabike akirabike-shape" src="./images/akira-bike.png"></img>
+                <img className="akirabike" src="./images/akira-bike.png"></img>            
                 <h1 className="akiratext akiragraphics">AKIRA</h1>
-                <img className="akira-explosion" src="/images/akira-explosion.png"></img>
+                <img className="akira-explosion" src="./images/akira-explosion.png"></img>
         </div>
     )
 }
